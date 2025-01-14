b1870916a83b0ebb961e53f4e69e47f9b696c1bf
Aayushma poudel <np03cs4a230485@heraldcollege.edu.np>
2025-01-14 08:07:23 +0545

    Adding getter method to student class

--- student.java
+++ student.java
@@ -1,13 +1,10 @@
 public class student {
     private String name;
     private int age;
-
-    // Constructor
     public student(String name, int age) {
         this.name = name;
         this.age = age;
     }
-
     // Getters
     public String getName() {
         return name;
